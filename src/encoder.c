988366316d28e0c9274b902060ad674b70c03719
Matthew Blythe <mblythester+git@gmail.com>
2013-10-27 20:39:17 -0600

    remove internal pullup on quadtrature decoder pins...I think this was causing too much noise.

--- src/encoder.c
+++ src/encoder.c
@@ -23,7 +23,7 @@ void encoder_init() {
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_6 | GPIO_Pin_7;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
 //  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_Init(GPIOA, &GPIO_InitStructure);
   GPIO_PinAFConfig(GPIOA, GPIO_PinSource0, GPIO_AF_2);
