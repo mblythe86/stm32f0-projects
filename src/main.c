c50722d200fdd7ef867ab57a1e6f5b15e1a9fff6
Matthew Blythe <mblythester+git@gmail.com>
2015-10-29 23:26:51 -0600

    Working the way I want, I think

--- src/main.c
+++ src/main.c
@@ -81,8 +81,11 @@ void SysTick_Handler(void) {
 }
 
 typedef enum {
-  SPIDER_AUTO,
-  SPIDER_MANUAL
+  SPIDER_AUTO_NEXT = 0,
+  SPIDER_AUTO_WAIT = 1,
+  SPIDER_MANUAL = 2,
+  SPIDER_RECORD = 3,
+  SPIDER_GO = 4
 } Spider_state_t;
 
 int main(void) {
@@ -136,8 +139,14 @@ int main(void) {
 
   // 100 "setpoint units" ~= 8 3/8 inches
   int counter = 0;
+  int savepoint = 0;
   int last_code = IR_ZERO;
   int tmp;
+  int savepoints1[10], savepoints2[10];
+  for(int i=0; i<10; i++){
+    savepoints1[i] = 0;
+    savepoints2[i] = 0;
+  }
   pid1.mySetpoint = 0;
   pid2.mySetpoint = 0;
   Spider_state_t state = SPIDER_MANUAL;
@@ -146,8 +155,8 @@ int main(void) {
     if(tmp != IR_NOT_NEW){
       last_code = tmp;
       if(tmp == IR_PLAY){
-        state = SPIDER_AUTO;
-        counter = 0;
+        state = SPIDER_AUTO_NEXT;
+				savepoint = 0;
       }
       else if(tmp == IR_STOP){
         state = SPIDER_MANUAL;
@@ -165,19 +174,84 @@ int main(void) {
         else if(tmp == IR_CHN_UP){
           pid1.mySetpoint += 10;
         }
+        else if(tmp == IR_REC){
+          state = SPIDER_RECORD;
+        }
+        else if(tmp == IR_GO){
+          state = SPIDER_GO;
+        }
+
+        if(pid1.mySetpoint > pid1.myInput + 20){
+          pid1.mySetpoint = pid1.myInput + 20;
+        }
+        else if(pid1.mySetpoint < pid1.myInput - 20){
+          pid1.mySetpoint = pid1.myInput - 20;
+        }
+        if(pid2.mySetpoint > pid2.myInput + 20){
+          pid2.mySetpoint = pid2.myInput + 20;
+        }
+        else if(pid2.mySetpoint < pid2.myInput - 20){
+          pid2.mySetpoint = pid2.myInput - 20;
+        }
+      }
+      else if(state == SPIDER_RECORD){
+        if(tmp == IR_REC){
+          //do nothing, easy to hit twice
+        }
+        else if (tmp >= IR_ZERO && tmp <= IR_NINE) {
+          savepoints1[tmp] = pid1.mySetpoint;
+          savepoints2[tmp] = pid2.mySetpoint;
+          state = SPIDER_MANUAL;
+        }
+        else{
+          state = SPIDER_MANUAL;
+        }
+      }
+      else if(state == SPIDER_GO){
+        if(tmp == IR_GO){
+          //do nothing, easy to hit twice
+        }
+        else if (tmp >= IR_ZERO && tmp <= IR_NINE) {
+          pid1.mySetpoint = savepoints1[tmp];
+          pid2.mySetpoint = savepoints2[tmp];
+          state = SPIDER_MANUAL;
+        }
+        else{
+          state = SPIDER_MANUAL;
+        }
       }
     }
     
-    if(state == SPIDER_AUTO){
-	    if(counter++ == 60){ //6 seconds-ish?
-		    pid1.mySetpoint += 100;
-		    pid2.mySetpoint += 100;
-	    }
-	    else if(counter == 120){
-		    pid1.mySetpoint -= 100;
-		    pid2.mySetpoint -= 100;
-		    counter = 0;
-	    }
+    if(state == SPIDER_AUTO_NEXT){
+      if (  savepoints1[savepoint] != 0 
+         && savepoints2[savepoint] != 0){
+        pid1.mySetpoint = savepoints1[savepoint];
+        pid2.mySetpoint = savepoints2[savepoint];
+        counter = 0;
+        state = SPIDER_AUTO_WAIT;
+      }
+      else{
+        savepoint++;
+        if(savepoint > 9){
+          savepoint = 0;
+        }
+      }
+    }
+    if(state == SPIDER_AUTO_WAIT){
+      int diff1 = savepoints1[savepoint] - pid1.myInput;
+      int diff2 = savepoints2[savepoint] - pid2.myInput;
+      if (  diff1 < 10 && diff1 > -10
+         && diff2 < 10 && diff2 > -10){
+         counter++;
+      }
+      if(counter > 60){ //about 6 sec?
+        counter = 0;
+        savepoint++;
+        if(savepoint > 9){
+          savepoint = 0;
+        }
+        state = SPIDER_AUTO_NEXT;
+      }
     }
 
     lcd_line_one();
@@ -193,12 +267,14 @@ int main(void) {
     lcd_write_int16(pos);
     lcd_write_string(" ");
     lcd_line_two();
-    lcd_write_string("pid set: ");
-    lcd_write_int16(pid1.mySetpoint);
+    lcd_write_string("state: ");
+    lcd_write_int16(state);
+    lcd_write_string(" pt: ");
+    lcd_write_int16(savepoint);
     lcd_write_string("   ");
     lcd_line_three();
-    lcd_write_string("pid out: ");
-    lcd_write_int16(pid1.myOutput);
+    lcd_write_string("counter: ");
+    lcd_write_int16(counter);
     lcd_write_string("   ");
     lcd_line_four();
     lcd_write_string("IR code: ");
